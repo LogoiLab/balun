f2013f7847a7e62fc16ce3860c81e9acae888016
Chad Baxter <cbax@doslabelectronics.com>
2023-05-17 23:38:55 -0400

    working on amisus perms

---
+++ src/commands/susban.rs
@@ -0,0 +1,75 @@
+use serenity::builder::CreateApplicationCommand;
+use serenity::model::prelude::command::CommandOptionType;
+use serenity::model::prelude::interaction::application_command::{
+    ApplicationCommandInteraction, CommandDataOptionValue,
+};
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+
+use crate::config::ConfigData;
+
+pub async fn run(ctx: &mut Context, command: &ApplicationCommandInteraction) -> String {
+    let to_be_susbanned = command
+        .data
+        .resolved
+        .users
+        .keys()
+        .next()
+        .expect("Could not get required data from command.");
+    let calling_member = command
+        .member
+        .clone()
+        .expect("failed to get command member.")
+        .user
+        .id;
+    println!("to be oped: {:?}", to_be_susbanned);
+    println!("calling member: {:?}", calling_member);
+    let option = command
+        .data
+        .options
+        .get(0)
+        .expect("Failed to get option data.")
+        .resolved
+        .as_ref()
+        .expect("Expected user object");
+    let mut data = ctx.data.write().await;
+    let config = data.get_mut::<ConfigData>().unwrap();
+    if config
+        .interaction
+        .operators
+        .contains(&i64::try_from(calling_member.as_u64().clone()).unwrap())
+    {
+        if let CommandDataOptionValue::User(_user, _member) = option {
+            for i in 0..config.interaction.operators.len() {
+                if config.interaction.operators.get(i).unwrap().clone()
+                    == to_be_deoped.as_u64().clone() as i64
+                {
+                    config.interaction.operators.remove(i);
+                    config.save();
+                    return format!(
+                        "<@{}> has been removed from the bot operators list by <@{}>.",
+                        to_be_deoped, calling_member
+                    );
+                }
+            }
+            return format!("User <@{}> not found in operators list.", to_be_deoped);
+        } else {
+            return "Please provide a valid user.".into();
+        }
+    } else {
+        return "You do not have permission to access that command.".into();
+    }
+}
+
+pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+    command
+        .name("deop")
+        .description("Disallows a privileged user from accessing privileged bot features.")
+        .create_option(|option| {
+            option
+                .name("user")
+                .description("The user to remove")
+                .kind(CommandOptionType::User)
+                .required(true)
+        })
+}
---
+++ src/commands/unsusban.rs
@@ -0,0 +1,79 @@
+use std::ops::Deref;
+
+use serenity::builder::CreateApplicationCommand;
+use serenity::model::prelude::command::CommandOptionType;
+use serenity::model::prelude::interaction::application_command::{
+    ApplicationCommandInteraction, CommandDataOptionValue,
+};
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+
+use crate::config::{Config, ConfigData};
+
+pub async fn run(ctx: &mut Context, command: &ApplicationCommandInteraction) -> String {
+    let to_be_oped = command
+        .data
+        .resolved
+        .users
+        .keys()
+        .next()
+        .expect("Could not get required data from command.");
+    let calling_member = command
+        .member
+        .clone()
+        .expect("failed to get command member.")
+        .user
+        .id;
+    let option = command
+        .data
+        .options
+        .get(0)
+        .expect("Failed to get option data.")
+        .resolved
+        .as_ref()
+        .expect("Expected user object");
+
+    let mut data = ctx.data.write().await;
+    let config = data.get_mut::<ConfigData>().unwrap();
+    if config
+        .interaction
+        .operators
+        .contains(&i64::try_from(calling_member.as_u64().clone()).unwrap())
+    {
+        if to_be_oped.as_u64() == calling_member.as_u64() {
+            return "You are already a bot operator.".into();
+        } else {
+            println!("to be oped: {:?}", to_be_oped);
+            println!("calling member: {:?}", calling_member);
+        }
+
+        if let CommandDataOptionValue::User(_user, _member) = option {
+            config
+                .interaction
+                .operators
+                .push(to_be_oped.as_u64().clone() as i64);
+            config.save();
+            return format!(
+                "<@{}> has been added to the bot operators list by <@{}>.",
+                to_be_oped, calling_member
+            );
+        } else {
+            return "Please provide a valid user".into();
+        }
+    } else {
+        return "You do not have permission to access that command.".into();
+    }
+}
+
+pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+    command
+        .name("op")
+        .description("Allow a user to use privileged bot features.")
+        .create_option(|option| {
+            option
+                .name("user")
+                .description("The user to add")
+                .kind(CommandOptionType::User)
+                .required(true)
+        })
+}
